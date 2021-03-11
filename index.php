6838f8a16b0107ba24f87817b775988cf9efd4bd
Dubeshcz <dub.tadeas@gmail.com>
2021-03-11 16:36:50 +0100

    WS 1.0.1
    Bootstrap nahrán

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <meta name="description" content="Website about World of Warcraft">
+        <meta name="keywords" content="Dubesh, WoW, World of Warcraft, Alliance">
+        <meta name="author" content="Dubesh">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>HOME | Dubesh</title>
+    </head>
+    <body>
+        <div class="container-fluid bg-danger mt-5 w-75">
+            <div class="col-12">
+                <img src="img/user.png">
+            </div>
+        </div>
+    </body>
+</html>
