e3f4a65034c36a5693efd020b9d6dc8840ffaa9b
Dubeshcz <dub.tadeas@gmail.com>
2021-03-11 17:06:13 +0100

    WS 1.0.2
    Form added

--- index.php
+++ index.php
@@ -9,9 +9,22 @@
         <title>HOME | Dubesh</title>
     </head>
     <body>
-        <div class="container-fluid bg-danger mt-5 w-75">
-            <div class="col-12">
-                <img src="img/user.png">
+        <div class="container bg-white mt-5">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                    <img src="img/user.png" height="250vh" class="mt-5 ms-3 mb-5">
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-12">
+                <form>
+                    <div class="form-group">
+                        <input type="email" class="form-control w-50 mt-3" name="username" placeholder="Enter email">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control w-50 mt-3" name="password" placeholder="Password">
+                    </div>
+                    <button type="submit" class="btn btn-primary w-50 mt-3 mb-3">Submit</button>
+                </form>
+                </div>
             </div>
         </div>
     </body>
