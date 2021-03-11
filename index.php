9fb800d37e7f12751a87b3cbcb85ddfcc180611a
Dubeshcz <dub.tadeas@gmail.com>
2021-03-11 17:20:08 +0100

    WS 1.0.3
    New styles

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     <head>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <meta name="description" content="Website about World of Warcraft">
         <meta name="keywords" content="Dubesh, WoW, World of Warcraft, Alliance">
         <meta name="author" content="Dubesh">
@@ -9,21 +10,22 @@
         <title>HOME | Dubesh</title>
     </head>
     <body>
-        <div class="container bg-white mt-5">
+        <div class="container bg-white mt-10 br-1 p-5">
             <div class="row">
                 <div class="col-lg-6 col-md-6 col-sm-12">
-                    <img src="img/user.png" height="250vh" class="mt-5 ms-3 mb-5">
+                    <img src="img/user.png" height="250vh" class="mt-5 ms-5 mb-5">
                 </div>
                 <div class="col-lg-6 col-md-6 col-sm-12">
-                <form>
-                    <div class="form-group">
-                        <input type="email" class="form-control w-50 mt-3" name="username" placeholder="Enter email">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control w-50 mt-3" name="password" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-primary w-50 mt-3 mb-3">Submit</button>
-                </form>
+                    <h1 class="mt-3 mb-5">Member Login</h1>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control w-50 mt-3" name="username" placeholder="Username">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control w-50 mt-3" name="password" placeholder="Password">
+                        </div>
+                        <button type="submit" class="btn btn-primary w-50 mt-4 mb-3">Submit</button>
+                    </form>
                 </div>
             </div>
         </div>
